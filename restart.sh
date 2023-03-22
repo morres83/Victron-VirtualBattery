df932c2861f1a3495e3546f9c3ce9b8595105634
morres83 <morres83@users.noreply.github.com>
2023-03-22 17:12:39 +0100

    changed filenames and scripts

--- restart.sh
+++ restart.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-kill $(pgrep -f "python $SCRIPT_DIR/virtualbattery_mqtt.py")
+kill $(pgrep -f "python $SCRIPT_DIR/dbus-virtualbattery_mqtt.py")
--- uninstall.sh
+++ uninstall.sh
@@ -3,6 +3,6 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 SERVICE_NAME=$(basename $SCRIPT_DIR)
 
 rm /service/$SERVICE_NAME
-kill $(pgrep -f 'supervise virtualbattery_mqtt')
+kill $(pgrep -f 'supervise dbus-virtualbattery_mqtt')
 chmod a-x $SCRIPT_DIR/service/run
 ./restart.sh
