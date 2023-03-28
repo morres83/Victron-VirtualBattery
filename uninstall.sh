afffe986f7e635542b6b734cf5fef129c473402b
morres83 <morres83@users.noreply.github.com>
2023-03-28 08:18:47 +0200

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -2,6 +2,9 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 SERVICE_NAME=$(basename $SCRIPT_DIR)
 
+sed -i '/dbus-virtualbattery_mqtt/d' /data/rc.local
+
 rm /service/$SERVICE_NAME
 kill $(pgrep -f 'supervise dbus-virtualbattery_mqtt')
 chmod a-x $SCRIPT_DIR/service/run
+./restart.sh
